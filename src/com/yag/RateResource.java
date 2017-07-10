9fca711bd1e65ddb51d1f259f2b734e6438228e8
Yagyank Chadha <yagyank@gmail.com>
2017-07-10 19:14:25 +0800

    Parameter bug not yet fixed

--- src/com/yag/RateResource.java
+++ src/com/yag/RateResource.java
@@ -1,21 +1,30 @@
 package com.yag;
 import javax.ws.rs.core.MediaType;
 
+import javax.ws.rs.core.Response;
+
+import java.io.Console;
+
+import javax.jws.WebParam;
 import javax.print.attribute.standard.Media;
+import javax.websocket.server.PathParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 @Path("/hello")
 public class RateResource {
 	
+
+	@Path("{id}")
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String sayHello() {
-		return "Hello";
+	public Response getMsg(@PathParam("id") String msg) {
+		System.out.println(msg+"none");
+		String output = "Jersey saya : " + msg;
+		return Response.status(200).entity(output).build();
 	}
 	
-	public static void main() {
-		System.out.println("hi");
-	}
+
+	
 }
